28bd41f47e51b5eb995fdf273ca3cfdcf5661697
Guilherme Vieira <super.driver.512@gmail.com>
2016-03-18 18:21:32 -0300

    Implement s3d_geom_full_screen_quad global.

---
+++ geom/full_screen_quad.c
@@ -0,0 +1,8 @@
+#include "full_screen_quad.h"
+
+s3d_vec2 s3d_geom_full_screen_quad[4] = {
+    -1, -1,
+    1, -1,
+    -1, 1,
+    1, 1
+};
---
+++ geom/full_screen_quad.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "../math/vec2.h"
+
+extern s3d_vec2 s3d_geom_full_screen_quad[4];
