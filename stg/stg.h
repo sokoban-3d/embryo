0b2cf1586e2604c3e5d9c04b575c21517f03156a
Guilherme Vieira <super.driver.512@gmail.com>
2016-03-23 03:53:02 -0300

    Implement s3d_stg model fields.

--- stg/stg.h
+++ stg/stg.h
@@ -3,6 +3,8 @@
 #include "../math/vec3.h"
 #include "../math/intvec2.h"
 
+#include "../model/model.h"
+
 #include "player.h"
 #include "wall.h"
 #include "block.h"
@@ -17,16 +19,26 @@ struct {
         float tsz;
     } board;
 
+    s3d_model model;
+
     s3d_stg_player player;
+
+    s3d_model player_model;
 
     int num_walls;
     s3d_stg_wall *walls;
 
+    s3d_model wall_model;
+
     int num_blocks;
     s3d_stg_block *blocks;
 
+    s3d_model block_model;
+
     int num_slots;
     s3d_stg_slot *slots;
+
+    s3d_model slot_model;
 
     int num_slotted_blocks;
 
