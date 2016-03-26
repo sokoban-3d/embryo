3cd638432b8fb03343d056cbb465e4cc623a961c
Guilherme Vieira <super.driver.512@gmail.com>
2016-03-26 04:40:55 -0300

    Implement s3d_stg_slot {pos,rot,scale,mat} fields.

--- stg/slot.h
+++ stg/slot.h
@@ -1,7 +1,15 @@
 #pragma once
 
 #include "../math/intvec2.h"
+#include "../math/vec3.h"
+#include "../math/mat4.h"
 
 typedef struct {
     s3d_intvec2 tpos;
+
+    s3d_vec3 pos;
+    s3d_vec3 rot;
+    s3d_vec3 scale;
+
+    s3d_mat4 mat;
 } s3d_stg_slot;
