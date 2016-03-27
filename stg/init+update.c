3001bef0cbe2552c85cb2e83f5d962264eb3c982
Guilherme Vieira <super.driver.512@gmail.com>
2016-03-27 19:45:58 -0300

    s3d_stg_init: Initialize all object scales to identity.

--- stg/init+update.c
+++ stg/init+update.c
@@ -8,6 +8,7 @@
 #include "../math/scale.h"
 #include "../math/euler_rot.h"
 #include "../math/translate.h"
+#include "../math/vec3_assign.h"
 
 #define pos_from_tpos(p, i, j) \
     s3d_stg.board.origin[i] + ( \
@@ -44,8 +45,20 @@ void s3d_stg_init() {
         }
     }
 
+    {
+        s3d_vec3_assign(s3d_stg.player.scale, 1, 1, 1);
+
+        for(int i = 0; i < s3d_stg.num_blocks; ++i) {
+            s3d_stg_block *b = &s3d_stg.blocks[i];
+
+            s3d_vec3_assign(b->scale, 1, 1, 1);
+        }
+    }
+
     for(int i = 0; i < s3d_stg.num_walls; ++i) {
         s3d_stg_wall *w = &s3d_stg.walls[i];
+
+        s3d_vec3_assign(w->scale, 1, 1, 1);
 
         pos_update(w);
         mat_update(w);
@@ -53,6 +66,8 @@ void s3d_stg_init() {
 
     for(int i = 0; i < s3d_stg.num_slots; ++i) {
         s3d_stg_slot *s = &s3d_stg.slots[i];
+
+        s3d_vec3_assign(s->scale, 1, 1, 1);
 
         pos_update(s);
         mat_update(s);
