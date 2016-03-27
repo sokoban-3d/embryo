6ff32ca7bb5e15b69d186a93ebea09585a813f9a
Guilherme Vieira <super.driver.512@gmail.com>
2016-03-27 18:42:05 -0300

    s3d_translate: Don't overwrite matrix values.

--- math/translate.cpp
+++ math/translate.cpp
@@ -5,5 +5,5 @@ cppfn void s3d_translate(s3d_mat4 mat, const s3d_vec3 val) {
     glm::mat4 *_mat = (glm::mat4 *)(mat);
     const glm::vec3 *_val = (const glm::vec3 *)(val);
 
-    *_mat = glm::translate(*_val);
+    *_mat *= glm::translate(*_val);
 }
