be27003dcf4c9295bb59988b636017dcaaf49e85
Guilherme Vieira <super.driver.512@gmail.com>
2016-03-27 20:18:46 -0300

    s3d_stg_{init,update}: Wrap macro arguments in parens to avoid precedence problems.

--- stg/init+update.c
+++ stg/init+update.c
@@ -12,19 +12,19 @@
 
 #define pos_from_tpos(p, i, j) \
     s3d_stg.board.origin[i] + ( \
-        p->tpos[j] * s3d_stg.board.tsz \
+        (p)->tpos[j] * s3d_stg.board.tsz \
     )
 
 #define pos_update(p) \
-    p->pos[0] = pos_from_tpos(p, 0, 0); \
-    p->pos[1] = s3d_stg.board.origin[1]; \
-    p->pos[2] = pos_from_tpos(p, 2, 1);
+    (p)->pos[0] = pos_from_tpos((p), 0, 0); \
+    (p)->pos[1] = s3d_stg.board.origin[1]; \
+    (p)->pos[2] = pos_from_tpos((p), 2, 1);
 
 #define mat_update(p) \
-    s3d_identity(p->mat); \
-    s3d_scale(p->mat, p->scale); \
-    s3d_euler_rot(p->mat, p->rot); \
-    s3d_translate(p->mat, p->pos);
+    s3d_identity((p)->mat); \
+    s3d_scale((p)->mat, (p)->scale); \
+    s3d_euler_rot((p)->mat, (p)->rot); \
+    s3d_translate((p)->mat, (p)->pos);
 
 void s3d_stg_init() {
     s3d_identity(s3d_stg.mat);
