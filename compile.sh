36647c9f774121fe6117097e1dd431da1da0c26f
Rafael G. Dantas <rafagd@gmail.com>
2016-04-01 23:27:41 -0300

    Adaptações pro Windows/MSYS

--- compile.sh
+++ compile.sh
@@ -2,5 +2,27 @@
 
 mkdir -p build
 cd build
-cmake ..
-make
+
+case $1 in
+    clean)
+    	rm -R *
+	;;
+    *)
+        CMAKE_GEN="UNIX Makefiles"
+        HAVE_INT64=
+
+        #IF INDOWS/MSYS
+        if [ -n "`uname -s | grep -v mingw`" ]; then
+            CMAKE_GEN="MSYS Makefiles"
+
+            #IF WIN64/MSYS
+            if [ -n "`uname -s | grep -v mingw64`" ]; then
+                echo ""
+                HAVE_INT64="-DHAVE_LONG_LONG_INT_64=1"
+            fi
+        fi
+
+        cmake -G "${CMAKE_GEN}" ${HAVE_INT64} ..
+        make
+	;;
+esac
